45b8332350bb9a0ea5cbdfe27095e7ac01880d3f
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-22 20:07:08 +0300

    Задание №4

--- src/Main.java
+++ src/Main.java
@@ -37,5 +37,12 @@ public class Main {
         System.out.println();
         float averageSpend = (float) sum / arr.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageSpend + " рублей");
+
+        //Задание №4
+        System.out.println();
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--){
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
